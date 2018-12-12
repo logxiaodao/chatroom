15f16aded660aa9a19cbafd74bf5d819ab284e89
long <1254624823@qq.com>
2018-12-12 19:36:32 +0800

    项目初始化

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: longxiangjun
+ * Date: 2018/12/11
+ * Time: 下午10:48
+ */
+
+namespace App\Http\Controllers;
+
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        echo "hello world";
+    }
+}
